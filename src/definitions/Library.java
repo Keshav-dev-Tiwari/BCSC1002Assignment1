57c7eee84cc9e7850265655e93863f41cd6c2e08
Keshav Dev Tiwari <keshav.tiwari_cs19@gla.ac.in>
2020-08-29 22:59:34 +0530

    added constructor and getter setter method for field

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -7,4 +7,25 @@
 package definitions;
 
 public class Library {
+    Book[]  booksAvailableInLibrary;
+
+    public Library() {
+        this.booksAvailableInLibrary = new Book[10];
+        for (int i = 0; i < booksAvailableInLibrary.length; i++) {
+            booksAvailableInLibrary[i] = new Book("Book Number: " + (i + 1));
+        }
+    }
+
+    public Library(Book[] booksAvailableInLibrary) {
+        this.booksAvailableInLibrary = booksAvailableInLibrary;
+    }
+
+    public Book[] getbooksAvailableInLibrary() {
+        return booksAvailableInLibrary;
+    }
+
+    public void setbooksAvailableInLibrary(Book[] booksAvailableInLibrary) {
+        this.booksAvailableInLibrary = booksAvailableInLibrary;
+    }
+
 }
