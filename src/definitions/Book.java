0381427eb808fab02962e584a51c58585d37a573
Keshav Dev Tiwari <keshav.tiwari_cs19@gla.ac.in>
2020-08-29 22:16:38 +0530

    adding constructor to Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -10,4 +10,23 @@ public class Book {
     private String nameOfTheBook;
     private String authorOfTheBook;
     private String ISBNnumberOfTheBook;
+
+    public  Book(){
+        nameOfTheBook = "theunlovedtales";
+        authorOfTheBook = "kaka";
+        ISBNnumberOfTheBook = "05042001";
+    }
+
+    public Book(String nameOfTheBook){
+        this.nameOfTheBook = nameOfTheBook;
+        this.authorOfTheBook = "Abhishek";
+        this.ISBNnumberOfTheBook = "123456789";
+    }
+
+    public Book(String nameOfTheBook, String authorOfTheBook, String ISBNnumberOfTheBook) {
+        this.nameOfTheBook = nameOfTheBook;
+        this.authorOfTheBook = authorOfTheBook;
+        this.ISBNnumberOfTheBook = ISBNnumberOfTheBook;
+
+    }
 }
